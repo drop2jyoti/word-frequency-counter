552f528be30901fcf107534e30cdd98ae77d8984
drop2jyoti <drop2jyoti@gmail.com>
2024-06-12 12:39:08 -0400

    Update counter.py
    Added file error handling

--- counter.py
+++ counter.py
@@ -1,43 +1,46 @@
 import matplotlib.pyplot as plt
 import string
 import numpy as np
-import sys
 
 # Read the file line by line and analyse the word frequency
 def count_frequency(file_location):
     # Create an empty dictionary
     d = dict()
     # Loop through each line of the file
-    with open(file_location, "r") as file:
-        for line in file:
-            # Remove the leading spaces and newline character
-            line = line.strip()
+    try:
+        with open(file_location, "r") as file:
+            for line in file:
+                # Remove the leading spaces and newline character
+                line = line.strip()
 
-            # Convert the characters in line to
-            # lowercase to avoid case mismatch
-            line = line.lower()
+                # Convert the characters in line to
+                # lowercase to avoid case mismatch
+                line = line.lower()
 
-            # Remove the punctuation marks from the line 
-            line = line.translate(line.maketrans("", "", string.punctuation)) 
-            # Remove the digits marks from the line 
-            line = line.translate(line.maketrans("", "", string.digits)) 
-            # Split the line into words
-            words = line.split(" ")
-            # Iterate over each word in line
-            for word in words:
-                if word == "":
-                    continue
-                # Check if the word is already in dictionary
-                if word in d:
-                    # Increment count of word by 1
-                    d[word] = d[word] + 1
-                else:
-                    # Add the word to dictionary with count 1
-                    d[word] = 1
+                # Remove the punctuation marks from the line 
+                line = line.translate(line.maketrans("", "", string.punctuation)) 
+                # Remove the digits marks from the line 
+                line = line.translate(line.maketrans("", "", string.digits)) 
+                # Split the line into words
+                words = line.split(" ")
+                # Iterate over each word in line
+                for word in words:
+                    if word == "":
+                        continue
+                    # Check if the word is already in dictionary
+                    if word in d:
+                        # Increment count of word by 1
+                        d[word] = d[word] + 1
+                    else:
+                        # Add the word to dictionary with count 1
+                        d[word] = 1
 
-    # sort the dictionary by the frequency count values, in Descending order
-    sortedDict = {k: v for k, v in sorted(d.items(), key=lambda item: item[1], reverse=True)}
-    return sortedDict
+        # sort the dictionary by the frequency count values, in Descending order
+        sortedDict = {k: v for k, v in sorted(d.items(), key=lambda item: item[1], reverse=True)}
+        return sortedDict
+    except IOError:
+            print("Invalid file location")
+            return None
 
 # feature to write the word and frequency into file  
 def write_to_file(sortedDict, file_location):  
@@ -48,9 +51,12 @@ def write_to_file(sortedDict, file_location):
  
  # make a new text file, then write user input to that
 def create_user_input_file(lines):  
-    with open("userText.txt", 'w') as f:
-        for item in lines:
-            f.write(item + '\n')
+    try:
+        with open("userText.txt", 'w') as f:
+            for item in lines:
+                f.write(item + '\n')
+    except IOError:
+            print("Error writing into the file from user input")
 
 #print the word and the frequency on console
 def print_the_words_with_frequency(sortedDict):
@@ -96,7 +102,9 @@ def main():
         print("\n")
         print_the_words_with_frequency(result)
         print("#####################################################")
+        print("########### Plotting the graph please wait.... ########")
         plot_graph(result)
+        print("########### Plotting finished ########")
     else:
         print("Result is unavailable")
        
